8e076a7558df97836cff39beb5e5740bbfd87d80
Ranjeet Kumar Jha <117855695+juniorRANJEET@users.noreply.github.com>
2024-06-12 23:43:22 +0900

    Create sum_of_even_number_for_loop.py

---
+++ sum_of_even_number_for_loop.py
@@ -0,0 +1,14 @@
+#summation of even number by using for loop
+total = 0
+
+for n in range (2,11,2):
+    total+=n
+print(total)
+
+
+#sum of odd nummber using for loop
+total = 0
+
+for n in range (1,11,2):
+    total+=n
+print(total)

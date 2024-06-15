ace259a49938f765939881732c1e606e87a80f35
Ranjeet Kumar Jha <117855695+juniorRANJEET@users.noreply.github.com>
2024-06-15 10:05:36 +0900

    Update fizz_buzz_game.py

--- fizz_buzz_game.py
+++ fizz_buzz_game.py
@@ -3,6 +3,7 @@ print("welcome to FizzBuzz game ")
 target = 100
 for number in range (1,target+1):
     if number % 15 == 0:
+    # if number % 3 ==0 and number % 5 == 0 :
         print("fizzbuzz")
     elif number % 3 == 0 :
         print("Fizz")
